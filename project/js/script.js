57b63d0d18176fb145e05a1cd16943df89f17f28
Veronikich <shtoler02va@gmail.com>
2021-10-01 02:41:47 +0300

    add

---
+++ project/js/script.js
@@ -0,0 +1,13 @@
+var childRadioClass = document.getElementById("leaf");
+console.log(childRadioClass);
+
+// function removeRadio(elem) {
+//     if (plantRadioArr[elem].getAttributeNames == 'checked') {
+//         plantRadioArr.removeAttribute('checked');
+//     }
+// }
+
+// plantRadioArr[0].onclick = function() {
+//     console.log(plantRadioArr[0].getAttributeNames);
+// }
+
